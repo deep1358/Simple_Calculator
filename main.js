7db18647ae72625604fc45a6f12fdc09d36fc67f
deep1358 <60952994+deep1358@users.noreply.github.com>
2020-11-09 16:52:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+var buttons = document.getElementsByClassName('button')
+var input = document.getElementById('input')
+
+for(let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", (e) => {
+      
+        input.value += e.target.textContent
+        
+        if(e.target.textContent === "="){
+            try{
+            if(input.value === ""){
+                input.value = ""
+            }
+            var d = input.value.replace("=","")
+            var e = d.replace("PI",Math.PI)
+            if(Number.isInteger(eval(e))){
+                input.value = eval(e)
+            }
+            else{
+                input.value = eval(e).toFixed(6)
+            }
+            }catch(e){
+                input.value="Wrong syntext"
+            }
+        }
+
+        if(e.target.textContent === "C"){
+            
+            input.value = ""
+        }
+
+        if(e.target.textContent === "sin"){
+            input.value = ""
+        }
+
+        if(e.target.textContent === "CE"){
+            input.value = input.value.slice(0,-3)
+        }
+
+       
+    })
+  }
+  
+
+
+
